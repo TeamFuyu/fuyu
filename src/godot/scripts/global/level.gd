b50f3ed448cd5f1154cea595a1136581f7a3e018
s94 <codeberg.woof674@passinbox.com>
2025-12-06 22:05:42 +0100

    fuyu remarche

--- src/godot/scripts/global/level.gd
+++ src/godot/scripts/global/level.gd
@@ -11,12 +11,14 @@ func _on_exit_left_body_entered(_body: Node2D) -> void:
 	queue_free()
 
 """
+"""
 func _on_exit_right_body_entered(_body: Node2D):
 	var next_scn=load(next_scn_right)
 	var next_scn_instance=next_scn.instantiate()
 	next_scn_instance.init_player_position(Vector2.RIGHT)
 	get_tree().root.call_deferred("add_child",next_scn_instance)
 	queue_free()
+"""
 """
 func init_player_position(direction: Vector2):
 	if direction == Vector2.RIGHT:
